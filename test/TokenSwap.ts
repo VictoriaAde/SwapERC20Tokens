70b6fce014b7c844f5b9e0fa3829308d4d56c087
Victoria <adedayovicky123@gmail.com>
2024-02-21 15:44:28 +0100

    TEST: successfully swapped vickish token to seyi

--- test/TokenSwap.ts
+++ test/TokenSwap.ts
@@ -19,7 +19,11 @@ describe("TokenSwap", function () {
 
     const vickishToken = await VickishToken.deploy();
     const seyiToken = await SeyiToken.deploy();
-    const tokenSwap = TokenSwap.deploy(vickishToken.target, seyiToken.target);
+    const tokenSwap = await TokenSwap.deploy(
+      vickishToken.target,
+      seyiToken.target
+    );
+
     return { tokenSwap, vickishToken, seyiToken, owner, otherAccount };
   }
 
@@ -35,5 +39,56 @@ describe("TokenSwap", function () {
     });
   });
 
-  describe("Withdrawals", function () {});
+  describe("swapVickishToken", function () {
+    it("Should successfully swap vickish token to seyi", async function () {
+      const { tokenSwap, vickishToken, seyiToken, otherAccount, owner } =
+        await loadFixture(deploySwapContractandTokens);
+
+      // need to approve contract to spend
+      console.log(tokenSwap.target);
+
+      // Approve contract to spend amount of tokens
+      // await vickishToken.connect(otherAccount).approve(tokenSwap.target, 200);
+      await vickishToken.approve(tokenSwap.target, 200);
+      await seyiToken.approve(tokenSwap.target, 200);
+
+      // Put amount into token pool of contract
+      await tokenSwap.poolVickish(200);
+      await tokenSwap.poolSeyi(200);
+
+      await vickishToken.transfer(otherAccount, 300);
+
+      const userBalVickish = await tokenSwap.checkUserBalOfvickishToken(
+        otherAccount
+      );
+      const userBalSeyi = await tokenSwap.checkUserBalOfseyiToken(otherAccount);
+
+      const contractBalVickish =
+        await tokenSwap.checkContractBalanceOfvickishToken();
+      const contractBalSeyi = await tokenSwap.checkContractBalanceOfseyiToken();
+
+      console.log("userBalVickish", userBalVickish);
+      console.log("userBalSeyi", userBalSeyi);
+      console.log("contractBalVickish", contractBalVickish);
+      console.log("contractBalSeyi", contractBalSeyi);
+
+      // approve contract to spend money from otherAccount which is msg.sender
+      await vickishToken.connect(otherAccount).approve(tokenSwap.target, 200);
+
+      // Swap vickish token to seyitoken
+      const swapVickish = await tokenSwap
+        .connect(otherAccount)
+        .swapVickishToken(100);
+
+      // check user bal for seyitoken to check if it was swapped successfully
+      const swapped = await tokenSwap.checkUserBalOfseyiToken(otherAccount);
+      expect(swapped).to.be.equal(100);
+
+      // expect user's bal of seyitoken to be equal the swapped amount
+      // console.log("swapped", swapped);
+
+      // console.log("userBalVickish", userBalVickish);
+      // console.log("userBalSeyi", userBalSeyi);
+    });
+  });
 });
